5b5fc282604c41c42c9b66054d0a6e0661d0201b
Saurabh Kacholiya <saurabhkacholiya@MacBook-Air.local>
2020-06-14 22:49:52 +0530

    complete the test

---
+++ myScript.js
@@ -0,0 +1,111 @@
+// progressive-image.js
+if (window.addEventListener && window.requestAnimationFrame && document.getElementsByClassName) window.addEventListener('load', function() {
+
+    // start
+    var pItem = document.getElementsByClassName('progressive replace'), timer;
+  
+    window.addEventListener('scroll', scroller, false);
+    window.addEventListener('resize', scroller, false);
+    inView();
+  
+  
+    // throttled scroll/resize
+    function scroller(e) {
+  
+      timer = timer || setTimeout(function() {
+        timer = null;
+        requestAnimationFrame(inView);
+      }, 300);
+  
+    }
+  
+  
+    // image in view?
+    function inView() {
+  
+      var wT = window.pageYOffset;
+      var wB = wT + window.innerHeight, cRect, pT, pB, p = 0;
+      while (p < pItem.length) {
+  
+        cRect = pItem[p].getBoundingClientRect();
+        pT = wT + cRect.top;
+        pB = pT + cRect.height;
+  
+        if (wT < pB && wB > pT) {
+          loadFullImage(pItem[p]);
+          pItem[p].classList.remove('replace');
+        }
+        else p++;
+      }
+  
+    }
+  
+  
+    // replace with full image
+    function loadFullImage(item) {
+  
+      if (!item || !item.href) return;
+  
+      // load image
+      var img = new Image();
+      if (item.dataset) {
+        img.srcset = item.dataset.srcset || '';
+        img.sizes = item.dataset.sizes || '';
+      }
+      img.src = item.href;
+      img.className = 'reveal';
+      if (img.complete) addImg();
+      else img.onload = addImg;
+  
+      // replace image
+      function addImg() {
+  
+        // disable click
+        item.addEventListener('click', function(e) { e.preventDefault(); }, false);
+  
+        // add full image
+        item.appendChild(img).addEventListener('animationend', function(e) {
+  
+          // remove preview image
+          var pImg = item.querySelector && item.querySelector('img.preview');
+          if (pImg) {
+            e.target.alt = pImg.alt || '';
+            item.removeChild(pImg);
+            e.target.classList.remove('reveal');
+          }
+  
+        });
+  
+      }
+  
+    }
+  
+  }, false);
+
+
+function deleteImg(id){
+  const divTag = document.getElementById(`${id}`)
+  divTag.style.display = "none";
+}
+
+function shuffle() {
+  var container = document.getElementById("main-container");
+  var elementsArray = Array.prototype.slice.call(container.getElementsByClassName('shuffleMe'));
+    elementsArray.forEach(function(element){
+    container.removeChild(element);
+  })
+  shuffleArray(elementsArray);
+  elementsArray.forEach(function(element){
+  container.appendChild(element);
+})
+}
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
